3e595341dfeeb05db366377a3e1b93d2b0e88787
William Banquier <william.banquier@GCCYJTLQJ1WK.local>
2019-09-20 15:26:05 -0400

    _

---
+++ csvPractice/csvReadingPractice.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+
+pizzaData = pd.read_csv('Datafiniti_Pizza_Restaurants_and_the_Pizza_They_Sell_May19.csv')
+
+print(car_data.columns)
+car_data.head(2)
