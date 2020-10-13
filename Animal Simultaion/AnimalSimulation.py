db0d8ac1022add135b572dafafc2e8af47d36ca4
William Banquier <william.banquier@ucc.on.ca>
2020-10-13 11:54:11 -0400

    Added new animal simulation code

---
+++ Animal Simultaion/AnimalSimulation.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+points = 2000
+
+reproduceValue = 1.2
+
+
+
+
+y = [x+1 for x in range(points)]
+x = []
+
+
+n = 0.5
+
+
+# print(reproduceValue * x * (1- x))
+for null in range(points):
+    n = reproduceValue * n * (1- n)
+    x.append(n)
+    # print(reproduceValue * x * (1- x))
+
+#Points is constanint across all animals, it represents time on the x axis.
+def createAnimal(reproduceValue, n, points):
+    x = [n]
+    for null in range(points):
+        n = reproduceValue * n * (1 - n)
+        x.append(n)
+    return x
+
+
+
+
+fig, ax = plt.subplots(figsize=(10,6))
+
+ax.scatter([0,1],[0,1] ,facecolor = "white", s = 1)
+
+
+# ax.scatter(y, createAnimal(2.4, 0.5, points), facecolor='black', s = 2, linestyle='-', marker='o')
+# ax.scatter(y, createAnimal(3.6, 0.5, points), facecolor='mediumvioletred', s = 2,  marker='o')
+
+#3.6321
+
+ax.plot(createAnimal(3.8, 0.4, points), linestyle='-', marker='o', color = "black")
+# ax.plot(createAnimal(2.8, 0.1, points), linestyle='-', marker='o', color = "lightgreen")
+
+plt.show()
+
+
+x = createAnimal (2, 0.4, points)
+y = []
+for i in range (len (x)):
+    y.append (x[i] - x[i-1])
+
+print(sum (y) / len(y))
